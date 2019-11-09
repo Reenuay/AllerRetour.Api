789a99d7f82157f520e9f277ad0cddc72a8ac479
Rustam Azizov <reenuay777@gmail.com>
2019-11-09 08:19:05 +0000

    Create basic server structure, add .gitignore

---
+++ Program.fs
@@ -0,0 +1,26 @@
+open System
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.DependencyInjection
+open Giraffe
+
+let webApp =
+  choose [
+    route "/" >=> text "It works!"
+  ]
+
+let configureApp (app : IApplicationBuilder) =
+  app.UseGiraffe webApp
+
+let configureServices (services : IServiceCollection) =
+  services.AddGiraffe() |> ignore
+
+[<EntryPoint>]
+let main _ =
+  WebHostBuilder()
+    .UseKestrel()
+    .Configure(Action<IApplicationBuilder> configureApp)
+    .ConfigureServices(configureServices)
+    .Build()
+    .Run()
+  0
