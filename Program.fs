a11c958b1c227c85c98b86b2e2e5762696e334aa
Rustam Azizov <reenuay777@gmail.com>
2019-11-25 15:19:37 +0000

    Add Serilog

--- Program.fs
+++ Program.fs
@@ -4,7 +4,7 @@ open Microsoft.AspNetCore.Builder
 open Microsoft.AspNetCore.Hosting
 open Microsoft.Extensions.Hosting
 open Microsoft.Extensions.Configuration
-
+open Serilog
 open Giraffe
 
 let webApp =
@@ -16,6 +16,7 @@ let webApp =
 let setHostConfig (basePath : string) (args : string array) (hostBuilder : IHostBuilder) =
   hostBuilder
     .UseContentRoot(basePath)
+    .UseSerilog()
     .ConfigureHostConfiguration(
       fun builder ->
         builder
@@ -30,12 +31,13 @@ let setAppConfig (hostBuilder : IHostBuilder) =
   hostBuilder
     .ConfigureAppConfiguration(
       fun context builder ->
-        let env = context.HostingEnvironment
+        let env = context.HostingEnvironment.EnvironmentName
+        let rootPath = context.HostingEnvironment.ContentRootPath
 
         builder
-          .SetBasePath(env.ContentRootPath)
+          .SetBasePath(rootPath)
           .AddYamlFile("appsettings.yml")
-          .AddYamlFile(sprintf "appsettings.%s.yml" env.EnvironmentName, true)
+          .AddYamlFile(sprintf "appsettings.%s.yml" env, true)
           .AddEnvironmentVariables("ASPNETCORE_")
           |> ignore
     )
@@ -58,9 +60,9 @@ let setGiraffeAppConfig (giraffeApp : HttpHandler ) (hostBuilder : IHostBuilder)
           )
           .Configure(
             fun context app ->
-              let env = context.HostingEnvironment
+              let env = context.HostingEnvironment.EnvironmentName
 
-              match env.EnvironmentName with
+              match env with
               | "Development" -> app.UseDeveloperExceptionPage()
               | _             -> app
               |> ignore
@@ -77,6 +79,19 @@ let run (host : IHost) = host.Run()
 [<EntryPoint>]
 let main args =
   let basePath = Directory.GetCurrentDirectory()
+
+  Log.Logger <-
+    LoggerConfiguration()
+      .Enrich.FromLogContext()
+      .WriteTo.Console()
+      .WriteTo.File(
+        "/var/log/aller_retour/api.log",
+        fileSizeLimitBytes = Nullable(1000000L),
+        rollOnFileSizeLimit = true,
+        shared =  true,
+        flushToDiskInterval = Nullable(TimeSpan.FromSeconds(1.0))
+      )
+      .CreateLogger()
 
   HostBuilder()
   |> setHostConfig basePath args
