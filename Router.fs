9935bdf4846c2f66e6157d4776e1d29c97661742
Rustam Azizov <reenuay777@gmail.com>
2019-12-14 10:12:25 +0000

    Use custom serializer

--- Router.fs
+++ Router.fs
@@ -12,7 +12,7 @@ let onError = function
   printfn "%A" e // TO DO: Add Logs!
   ServerErrors.INTERNAL_ERROR "Oops! Something went wrong..."
 
-let toHandler = map2 Successful.OK onError
+let toHandler = map2 (Json.serialize >> Successful.OK) onError
 
 let register input =
   warbler (fun _ ->
