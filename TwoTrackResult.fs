c1584feff0c2b23bd55e8b7b1b94c986e02c294e
Rustam Azizov <reenuay777@gmail.com>
2019-12-29 18:21:12 +0000

    Remove unnecessary functions

--- TwoTrackResult.fs
+++ TwoTrackResult.fs
@@ -25,6 +25,13 @@ module TwoTrackResult =
 
   let map f = either (f >> succeed) (fail)
 
+  let tee f x =
+    f x |> ignore
+    x
+
+  let eitherTeeResult fSuccess fFailure
+    = either (tee fSuccess >> succeed) (tee fFailure >> fail)
+
   let mult fSuccess fFailure switch1 switch2 x =
     match (switch1 x),(switch2 x) with
     | Success s1, Success s2 -> Success (fSuccess s1 s2)
@@ -32,18 +39,10 @@ module TwoTrackResult =
     | Success _,  Failure f2 -> Failure f2
     | Failure f1, Failure f2 -> Failure (fFailure f1 f2)
 
-  let tryCatch fFailure f x =
-    try
-      f x |> Success
-    with
-    | x -> fFailure x.Message |> Failure
-
-  let tee f x =
-    f x |> ignore
-    x
-
-  let eitherTeeResult fSuccess fFailure
-    = either (tee fSuccess >> succeed) (tee fFailure >> fail)
+  let (++) v1 v2 x =
+    let ok _ _ = x
+    let error e1 e2 = e1 @ e2
+    mult ok error v1 v2 x
 
   let resultIf rSuccess rFailure = function
   | true  -> Success rSuccess
@@ -53,13 +52,8 @@ module TwoTrackResult =
 
   let chain predicate rFailure x = resultIf x rFailure (predicate x)
 
-  let adapt switch map x = x |> map |> switch |> (either (fun _ -> succeed x) fail)
+  let adapt switch map x = x |> map |> switch |> either (fun _ -> succeed x) fail
 
   let failIfNone rFailure = function
   | Some x -> Success x
   | None   -> Failure rFailure
-
-  let (++) v1 v2 x =
-    let ok _ _ = x
-    let error e1 e2 = e1 @ e2
-    mult ok error v1 v2 x
