07bf07738ff4654d90b03fc324d250cdc18b8aba
Rustam Azizov <reenuay777@gmail.com>
2020-01-22 14:05:13 +0000

    Give more appropriate name to the module

---
+++ PrimitiveFunctions.fs
@@ -0,0 +1,14 @@
+module AllerRetour
+let tee f x =
+  f x |> ignore
+  x
+
+let tryCatch fSuccess fError f x =
+  try
+    x
+    |> f
+    |> fSuccess
+  with
+  | ex -> fError ex.Message
+
+let ignore2 x _ = x
