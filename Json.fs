1b4e90e25719db993e0bdc569c11c1274ded15a8
Rustam Azizov <reenuay777@gmail.com>
2020-02-10 22:31:13 +0000

    Don't distinguish strings

--- Json.fs
+++ Json.fs
@@ -2,9 +2,6 @@ module AllerRetour.Json
 
 open Microsoft.FSharpLu.Json
 
-let serialize (x: obj) =
-  match x with
-  | :? string as s -> s
-  | _ -> Compact.Strict.serialize x
+let serialize x = Compact.Strict.serialize x
 
 let inline deserialize s = Compact.Strict.tryDeserialize s
